be8ac755882916b149af059559ac50a49158bac7
Mike Liu <mikelkl@foxmail.com>
2016-07-01 10:37:59 +0800

    最简单的Servlet实例 - Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	private String message;
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+		message = "Hello World";
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 设置响应内容类型
+		response.setContentType("text/html");
+		
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+}
