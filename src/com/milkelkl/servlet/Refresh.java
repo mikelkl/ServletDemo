220960ae7a82a6f94367cfd1892f21480cebb6cf
Mike Liu <mikelkl@foxmail.com>
2016-07-01 17:20:13 +0800

    Servlet服务器 HTTP 响应

---
+++ src/com/milkelkl/servlet/Refresh.java
@@ -0,0 +1,82 @@
+package com.milkelkl.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Refresh extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 设置刷新自动加载时间为 5 秒
+		response.setIntHeader("Refresh", 5);
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		// Get current time
+		Calendar calendar = new GregorianCalendar();
+		String am_pm;
+		int hour = calendar.get(Calendar.HOUR);
+		int minute = calendar.get(Calendar.MINUTE);
+		int second = calendar.get(Calendar.SECOND);
+		if (calendar.get(Calendar.AM_PM) == 0) {
+			am_pm = "AM";
+		} else {
+			am_pm = "PM";
+		}
+
+		String CT = hour + ":" + minute + ":" + second + " " + am_pm;
+
+		PrintWriter out = response.getWriter();
+		String title = "自动刷新 Header 设置";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
+				+ "transitional//en\">\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title
+				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
+				+ "<h1 align=\"center\">" + title + "</h1>\n" + "<p>当前时间是："
+				+ CT + "</p>\n");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
