ba145d8039d346d0f4164f53ccb60f2d7a98a8cc
Mike Liu <mikelkl@foxmail.com>
2016-07-01 17:06:23 +0800

    Servlet 客户端 HTTP 请求

---
+++ src/com/milkelkl/servlet/DisplayHeader.java
@@ -0,0 +1,76 @@
+package com.milkelkl.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DisplayHeader extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String title = "HTTP Header 请求实例";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
+				+ "transitional//en\">\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title
+				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
+				+ "<h1 align=\"center\">" + title + "</h1>\n"
+				+ "<table width=\"100%\" border=\"1\" align=\"center\">\n"
+				+ "<tr bgcolor=\"#949494\">\n"
+				+ "<th>Header 名称</th><th>Header 值</th>\n" + "</tr>\n");
+
+		Enumeration headerNames = request.getHeaderNames();
+
+		while (headerNames.hasMoreElements()) {
+			String paramName = (String) headerNames.nextElement();
+			out.print("<tr><td>" + paramName + "</td>\n");
+			String paramValue = request.getHeader(paramName);
+			out.println("<td> " + paramValue + "</td></tr>\n");
+		}
+
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
