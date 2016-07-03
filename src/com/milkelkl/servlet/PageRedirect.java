56ce1ddbd9cda1bb9e87415285a70dc269d20090
Mike Liu <mikelkl@foxmail.com>
2016-07-03 16:13:11 +0800

    Servlet 网页重定向

---
+++ src/com/milkelkl/servlet/PageRedirect.java
@@ -0,0 +1,35 @@
+package com.milkelkl.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PageRedirect extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		
+		// 要重定向的新位置
+		String site=new String("http://www.baidu.com");
+		
+		response.setStatus(response.SC_MOVED_TEMPORARILY);
+		response.setHeader("Location", site);
+	}
+
+}
