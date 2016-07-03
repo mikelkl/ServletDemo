d6e5550e675bdd4dac088d55e04a4ad7282c71fa
Mike Liu <mikelkl@foxmail.com>
2016-07-03 16:08:54 +0800

    Servlet 处理日期

---
+++ src/com/milkelkl/servlet/CurrentDate.java
@@ -0,0 +1,49 @@
+package com.milkelkl.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CurrentDate extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String title = "显示当前的日期和时间";
+		Date dNow = new Date();
+		SimpleDateFormat ft = new SimpleDateFormat(
+				"E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
+				+ "transitional//en\">\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title
+				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
+				+ "<h1 align=\"center\">" + title + "</h1>\n"
+				+ "<h2 align=\"center\">" + ft.format(dNow) + "</h2>\n"
+				+ "</body></html>");
+		out.flush();
+		out.close();
+	}
+
+}
